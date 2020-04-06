f72bd59458e6dbcb5c32c133c8770749fb3ca1ef
Brett Israelsen <brett.israelsen@gmail.com>
2020-04-06 14:07:37 -0400

    moved map data-structure to it's own file

--- new_locations.py
+++ new_locations.py
@@ -23,30 +23,10 @@ class Locations:
     world = None
     """
     Nbrs: map from each direction d to a map of each location loc that has a neighbor in direction
-    d to that neighbor 
+    d to that neighbor
     """
     Nbrs = []
     AllLocations = set()
-    DN = Directions.N
-    DS = Directions.S
-    DE = Directions.E
-    DW = Directions.W
-
-    SandR_Locs = {"LH1":{DN:"LH2",DE:"XHL2"},"LH2":{DN:"LH3",DS:"LH1",DE:"203"},"LH3":{DE:"205"},\
-            "XHL2":{DN:"201",DE:"XHL1",DW:"LH1"},"XHL1":{DN:"J",DE:"XHC",DW:"XHL2"},"XHC":{DN:"CH1",\
-            DS:"BH1",DE:"XHR",DW:"XHL1"},"XHR":{DN:"RH1",DW:"XHC"},\
-            "CH1":{DN:"CH2",DS:"XHC",DE:"209"},"CH2":{DN:"CH3",DS:"CH1",DE:"211",DW:"208S"},\
-            "CH3":{DN:"CH4",DS:"CH2",DW:"208N"},"CH4":{DS:"CH3",DE:"215",DW:"210"},\
-            "RH1":{DN:"RH2",DS:"XHR",DW:"216S"},"RH2":{DN:"RH3",DS:"RH1",DW:"216N"},\
-            "RH3":{DN:"RH4",DS:"RH2",DW:"218"},"RH4":{DS:"RH3",DW:"220"},\
-            "BH1":{DN:"XHC",DE:"E1",DW:"MR"},"BH2":{DN:"BH1",DE:"E2",DW:"WR"},"MR":{DE:"BH1"},\
-            "WR":{DE:"BH2"},"E1":{DW:"BH1"},"E2":{DW:"BH2"},"201":{DN:"203",DS:"XHL2"},\
-            "203":{DS:"201",DE:"208S",DW:"LH2"},"205":{DN:"207",DW:"LH3"},"207":{DS:"205",DE:"210"},\
-            "210":{DE:"CH4",DW:"207"},"208N":{DS:"208S",DE:"CH3"},"208S":{DS:"208N",DE:"CH3"},\
-            "RJ":{DS:"XHL1"},"209":{DE:"216S",DW:"CH1"},"211":{DN:"213",DW:"CH2"},\
-            "213":{DS:"211",DE:"218"},"215":{DE:"220",DW:"CH4"},"220":{DE:"RH4",DW:"215"},\
-            "218":{DE:"RH3",DW:"213"},"216N":{DS:"216S",DE:"RH2"},\
-            "216S":{DN:"216N",DE:"RH1",DW:"209"}}
 
     def makeMap(pairsList):
         """
@@ -85,7 +65,7 @@ class Locations:
             Locations.world.setState(human.name, 'seenloc_' + str(initLoc), True)
 
         ## Make move actions
-        Locations.__makeMoveActions(human)        
+        Locations.__makeMoveActions(human)
         Locations.__makeExplorationBonus(human)
 
     def __makeHasNeighborDict(locKey, direction, locsWithNbrs):
@@ -174,16 +154,16 @@ class Locations:
 
     def move(human, direction):
         Locations.world.step(Locations.moveActions[human.name][direction])
-        
+
     def getDirection(src, dest):
         for d in range(4):
             if (src in Locations.Nbrs[d].keys()) and (Locations.Nbrs[d][src] == dest):
                 return d
         print('Source cannot reach dest', src, dest)
         return -1
-    
+
     def moveToLocation(human, src, dest):
        Locations.world.step(Locations.getMoveAction(human, src, dest))
-        
-    def getMoveAction(human, src, dest):        
+
+    def getMoveAction(human, src, dest):
         Locations.moveActions[human.name][Locations.getDirection(src, dest)]
