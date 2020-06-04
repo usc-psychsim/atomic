e84b51fa85321d8b162eb10442922830fe5e43dd
David V. Pynadath <david@pynadath.net>
2020-06-04 01:49:36 -0700

    Created test functions for pytest

--- test/test_actionEffect.py
+++ test/test_actionEffect.py
@@ -8,40 +8,43 @@ from psychsim.world import World, WORLD
 from psychsim.pwl import stateKey, actionKey
 from new_locations_fewacts import Locations, Directions
 from victims_fewacts import Victims
-from helpers import testMMBelUpdate, setBeliefs, setBeliefsNoVics
+from helpers import runMMBelUpdate, setBeliefs, setBeliefsNoVics
+
+def test_actionEffects():
+	# MDP or POMDP
+	Victims.FULL_OBS = True
+
+	world = World()
+
+	triageAgent = world.addAgent('TriageAg1')
+	agent = world.addAgent('ATOMIC')
 
 
-# MDP or POMDP
-Victims.FULL_OBS = True
+	VICTIMS_LOCS = ['E1']
+	VICTIM_TYPES = ['Orange']
+	Victims.world = world
+	Victims.makeVictims(VICTIMS_LOCS, VICTIM_TYPES, [triageAgent.name], ['BH1','E1'])
+	Victims.makePreTriageActions(triageAgent)
+	Victims.makeTriageAction(triageAgent)
 
-world = World()
+	Victims.P_VIC_FOV = (1.0 - Victims.P_EMPTY_FOV) / len(Victims.victimAgents)
 
-triageAgent = world.addAgent('TriageAg1')
-agent = world.addAgent('ATOMIC')
+	################# Locations and Move actions
+	Locations.EXPLORE_BONUS = 0
+	Locations.world = world
+	Locations.makeMapDict({'BH1':{Directions.E:'E1'}, 'E1':{Directions.W:'BH1'}})
+	Locations.makePlayerLocation(triageAgent, "E1")
 
+	## These must come before setting triager's beliefs
+	world.setOrder([{triageAgent.name}])
 
-VICTIMS_LOCS = ['E1']
-VICTIM_TYPES = ['Orange']
-Victims.world = world
-Victims.makeVictims(VICTIMS_LOCS, VICTIM_TYPES, [triageAgent.name], ['BH1','E1'])
-Victims.makePreTriageActions(triageAgent)
-Victims.makeTriageAction(triageAgent)
+	## Set players horizons
+	triageAgent.setAttribute('horizon',4)
 
-Victims.P_VIC_FOV = (1.0 - Victims.P_EMPTY_FOV) / len(Victims.victimAgents)
+	assert world.getState(triageAgent.name,'loc',unique=True) == 'E1'
+	####### Test if action effects are back
+	Locations.move(triageAgent, Directions.W)
+	assert world.getState(triageAgent.name,'loc',unique=True) == 'BH1'
 
-################# Locations and Move actions
-Locations.EXPLORE_BONUS = 0
-Locations.world = world
-Locations.makeMapDict({'BH1':{Directions.E:'E1'}, 'E1':{Directions.W:'BH1'}})
-Locations.makePlayerLocation(triageAgent, "E1")
-
-## These must come before setting triager's beliefs
-world.setOrder([{triageAgent.name}])
-
-## Set players horizons
-triageAgent.setAttribute('horizon',4)
-
-####### Test if action effects are back
-Locations.move(triageAgent, Directions.W)
-world.printState(beliefs=False)
-
+if __name__ == '__main__':
+	test_actionEffects()
