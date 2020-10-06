4acb82752819ae329c5dc852e9a59e581de4dfd4
skenny <skenny@ict.usc.edu>
2020-10-06 13:50:57 -0700

    filters all messages before mission start & after end filters all messages generated during pauses returns all necessary fields for use by psychsim for triage & door events

--- atomic/parsing/message_reader.py
+++ atomic/parsing/message_reader.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# read necessary portions of message bus for psychsim
+# read necessary portions of message buffer for psychsim
 # can call on entire file or request latest event
 
 from builtins import range
@@ -17,7 +17,7 @@ class room(object):
     def __init__(self, name, x0, z0, x1, z1):
         self.name = name
 
-        self.xrange = range(x0,x1) # add 1 to range here too?? likely not cuz edge will be wall
+        self.xrange = range(x0,x1) 
         self.zrange = range(z0,z1)
 
     def in_room(self, _x, _z):
@@ -39,13 +39,22 @@ class door(object):
         else:
             return False
 
+class msg(object):
+    def __init__(self, msg_type):
+        self.mtype = msg_type
+        self.mdict = {}
+
 class msgreader(object):
     def __init__(self, fname, latest=False):
-        self.psychsim_tags = ['mission_timer']
+        self.psychsim_tags = ['mission_timer', 'sub_type'] # maybe don't need here
         self.nmessages = 0
         self.rooms = []
         self.doors = []
- 
+        self.msg_types = ['Event:Triage', 'Event:Door']
+        self.messages = []
+        self.mission_running = False
+        
+
     def get_all_messages(self,fname):
         message_arr = []
         jsonfile = open(fname, 'rt')
@@ -57,24 +66,27 @@ class msgreader(object):
             jsonfile.close()
         return message_arr
 
-    def get_message_debug(self,jtxt):
-        print('****getting info*******')
-        obs = json.loads(jtxt)
-        message = obs[u'msg']
-        data = obs[u'data']
-        alldat = {}
-        print("printing single message....")
-        for (k,v) in data.items():
-            if k in self.psychsim_tags:
-                alldat[k] = v
-        for (k,v) in message.items():
-            if k in self.psychsim_tags:
-                alldat[k] = v
-        for (k,v) in alldat.items(): 
-            print("key: "+k)
-            print("val: "+str(v))
+    # add to msgreader obj
+    # TODO: add counter to know nlines btwn start/stop
+    def add_all_messages(self,fname):
+        message_arr = []
+        jsonfile = open(fname, 'rt')
+        nlines = 0
+        for line in jsonfile.readlines():
+            if line.find("mission_state\":\"Start") > -1:
+                self.mission_running = True
+            elif line.find("mission_state\":\"Stop") > -1:
+                self.mission_running = False
+            elif line.find("paused\":true") > -1:
+                self.mission_running = False
+            elif line.find("paused\":false") > -1:
+                self.mission_running = True
+            elif line.find('data') > -1:
+                self.add_message(line)
+            nlines += 1
+        jsonfile.close()
 
-    # TODO: add messages to msg reader instance instead of returning directly
+    # parses single message from line of json txt
     def parse_message(self,jtxt):
         msg_type = self.check_type(jtxt) # this should set psychsim_tags
         obs = json.loads(jtxt)
@@ -90,22 +102,55 @@ class msgreader(object):
         if msg_type in ['triage', 'beep', 'motionz', 'lever']:
             self.add_room(alldat,msg_type)            
         elif msg_type in ['door']: 
-#            self.add_room(alldat,msg_type)            
             self.add_door_rooms(alldat,msg_type)
         return alldat
+
+    # adds single message to msgreader.messages list
+    def add_message(self,jtxt): 
+        msg_type = self.check_type(jtxt) # this should set psychsim_tags
+        if msg_type in self.msg_types and self.mission_running:
+            m = msg(msg_type)
+            obs = json.loads(jtxt)
+            message = obs[u'msg']
+            data = obs[u'data']
+            m.mdict = {}
+            for (k,v) in data.items():
+                if k in self.psychsim_tags:
+                    m.mdict[k] = v
+            for (k,v) in message.items():
+                if k in self.psychsim_tags:
+                    m.mdict[k] = v
+            if msg_type in ['Event:Triage', 'Event:Lever']:
+                self.add_room(m.mdict,msg_type)            
+            elif msg_type == 'Event:Door':
+                self.add_door_rooms(m.mdict,msg_type)
+            self.messages.append(m)
 
     # adds which room event is occurring in 
     def add_room(self, msgdict, msg_type):
         x = 0
         z = 0
+        xkey = ''
+        ykey = ''
+        zkey = ''
+        room_name = ''
         for (k,v) in msgdict.items():
             if k.find('_x') > -1:
                 x = float(v)
+                xkey = k
             elif k.find('_z') > -1:
                 z = int(v)
+                zkey = k
+            elif k.find('_y') > -1:
+                ykey = k
         for r in self.rooms:
             if r.in_room(x,z):
-                msgdict.update({'room':r.name})
+                room_name = r.name
+        del msgdict[xkey]
+        del msgdict[ykey]
+        del msgdict[zkey]
+
+        msgdict.update({'room':room_name})
 
     def add_door_rooms(self, msgdict, msg_type):
         doors_found = 0
@@ -136,42 +181,14 @@ class msgreader(object):
     # if doesn't match any, we don't care about it so tag list will be empty
     # and message won't be processed
     def check_type(self,jtxt):
-        msg_type = ''
+        msg_type = 'NONE'
+        self.psychsim_tags = ['mission_timer', 'sub_type', 'playername']
         if jtxt.find('Event:Triage') > -1:
-            self.psychsim_tags = ['sub_type', 'triage_state', 'color', 'victim_x', 'victim_y', 'victim_z', 'timestamp', 'playername']
-            msg_type = 'triage'
-        elif jtxt.find('Event:MissionState') > -1:
-            self.psychsim_tags = ['mission_state', 'mission']
-            msg_type = 'mission'
+            self.psychsim_tags += ['triage_state', 'color', 'victim_x', 'victim_y', 'victim_z']
+            msg_type = 'Event:Triage'
         elif jtxt.find('Event:Door') > -1:
-            self.psychsim_tags += ['playername', 'open', 'door_x', 'door_y', 'door_z', 'room1', 'room2']
-            msg_type = 'door'
-        elif jtxt.find('Event:Beep') > -1:
-            self.psychsim_tags = ['playername', 'mission_timer', 'beep_x', 'beep_y', 'beep_z', 'message']
-            msg_type = 'beep'
-        elif jtxt.find('Event:Pause') > -1:
-            self.psychsim_tags = ['mission_timer', 'paused']
-            msg_type = 'pause'
-        elif jtxt.find('Event:Location') > -1: # not seeing entered_locations in current tria data thus far
-            self.psychsim_tags = ['playername', 'exited_locatoins', 'connected_locations']
-            msg_type = 'location'
-        elif jtxt.find('Event:VictimsExpired') > -1:
-            self.psychsim_tags += ['message']
-            msg_type = 'victimsexpired'
-        elif jtxt.find('Mission:VictimList') > -1:
-            self.psychsim_tags = ['mission_victim_list', 'room_name', 'message_type']
-            msg_type = 'victimlist'
-        elif jtxt.find('FoV') > -1:
-            self.psychsim_tags = ['playername', 'sub_type', 'observation', 'blocks']
-            msg_type = 'fov'
-        elif jtxt.find('motion_z') > -1: # for type 'state' to disambiguate
-            self.psychsim_tags = ['playername', 'sub_type', 'motion_x', 'motion_y', 'motion_z', 'message_type', 'yaw', 'pitch', 'life']
-            msg_type = 'motionz'
-        elif jtxt.find('Event:Lever') > -1:
-            self.psychsim_tags = ['playername', 'mission_time', 'powered', 'lever_x', 'lever_y', 'lever_z']
-            msg_type = 'lever'
-        else:
-            self.psychsim_tags = []
+            self.psychsim_tags += ['open', 'door_x', 'door_y', 'door_z', 'room1', 'room2']
+            msg_type = 'Event:Door'
         return msg_type
 
     # this will be updated to use mmap, for now reads all lines
@@ -220,8 +237,7 @@ jsonfile = '/home/skenny/usc/asist/data/study-1_2020.08_TrialMessages_CondBtwn-N
 reader = msgreader(jsonfile, True)
 reader.load_rooms('/home/skenny/usc/asist/data/ASIST_FalconMap_Rooms_v1.1_OCN.csv')
 reader.load_doors('/home/skenny/usc/asist/data/ASIST_FalconMap_Portals_v1.1_OCN.csv')
-singlemsg = reader.get_latest_message(jsonfile)
-print("LAST MESSAGE "+str(singlemsg))
-allmsgs = reader.get_all_messages(jsonfile)
-print("ALL MSGS: "+str(allmsgs))
-
+reader.add_all_messages(jsonfile)
+# print all the messages
+for m in reader.messages:
+    print(str(m.mdict))
