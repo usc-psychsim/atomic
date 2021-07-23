798a6bd14b11c00088222ad5719c3e114b54f695
Pedro Sequeira <pedrodbs@gmail.com>
2021-07-23 15:30:41 -0700

    small change to convert rddl file from template

--- examples/make_rddl_instance.py
+++ examples/make_rddl_instance.py
@@ -12,7 +12,7 @@ from atomic.parsing.map_parser import read_semantic_map
 from atomic.definitions import GOLD_STR, GREEN_STR
 
 
-#with open('../data/rddl_psim/victims.pickle', 'wb') as f:    
+# with open('../data/rddl_psim/victims.pickle', 'wb') as f:
 #    pickle.dump(msg_qs.jsonParser.vList, f)
 
 def generate_rddl_map(rooms, edges):
@@ -22,13 +22,14 @@ def generate_rddl_map(rooms, edges):
     loc_str = ','.join(rooms)
     return loc_str, nbr_str
 
+
 def generate_rddl_victims(victim_pickle):
-    with open(victim_pickle, 'rb') as f:    
+    with open(victim_pickle, 'rb') as f:
         vList = pickle.load(f)
-    
+
     reg_dict = {}
     crit_dict = {}
-    
+
     for vic in vList:
         rm = vic['room_name']
         if rm == '':
@@ -37,10 +38,10 @@ def generate_rddl_victims(victim_pickle):
             d = crit_dict
         else:
             d = reg_dict
-        
+
         if rm not in d.keys():
             d[rm] = 0
-        d[rm] = d[rm] +1
+        d[rm] = d[rm] + 1
 
     vic_str = ''
     for rm, ctr in crit_dict.items():
@@ -49,25 +50,32 @@ def generate_rddl_victims(victim_pickle):
         vic_str = vic_str + 'vcounter_unsaved_regular(%s) = %d;\n' % (rm, ctr)
     return vic_str
 
+
 def make_rddl_inst(rooms, edges,
-                   victim_pickle = '../data/rddl_psim/victims.pickle',                    
-                    rddl_template =  '../data/rddl_psim/sar_mv_tr_template.rddl',
-                    inst_name = 'inst1'):
+                   victim_pickle='../data/rddl_psim/victims.pickle',
+                   rddl_template='../data/rddl_psim/sar_mv_tr_template.rddl',
+                   rddl_out='../data/rddl_psim/sar_mv_tr_inst1.rddl'):
     ''' Create a RDDL instance from a RDDL template containing everything but the locations and adjacency info 
         which are obtained from a semantic map.    '''
 
-
     loc_str, nbr_str = generate_rddl_map(rooms, edges)
     vic_str = generate_rddl_victims(victim_pickle)
-    
+
     rddl_temp_file = open(rddl_template, "r")
-    
+
     rddl_str = rddl_temp_file.read()
     rddl_str = rddl_str.replace('LOCSTR', loc_str).replace('NBRSTR', nbr_str).replace('VICSTR', vic_str)
-    
-    rddl_out = rddl_template.replace('template', inst_name)
+
     rddl_inst_file = open(rddl_out, "w")
     rddl_inst_file.write(rddl_str)
-    
+
     rddl_temp_file.close()
     rddl_inst_file.close()
+
+
+if __name__ == '__main__':
+    rooms, edges = read_semantic_map('atomic/maps/Saturn/Saturn_1.4_3D_sm_v1.0.json')
+    make_rddl_inst(rooms, edges,
+                   'atomic/data/rddl_psim/victims.pickle',
+                   'atomic/data/rddl_psim/role_fol_template.rddl',
+                   'atomic/data/rddl_psim/role_big_fol.rddl')
