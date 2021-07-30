af1ad2527a907009487d9df358032c6d63f11842
David V. Pynadath <david@pynadath.net>
2021-07-30 03:07:17 -0700

    Transitioning to multiplayer version

--- atomic/inference.py
+++ atomic/inference.py
@@ -27,47 +27,45 @@ def make_observer(world, team, name='ATOMIC'):
     return agent
 
 
-def set_player_models(world, observer_name, player_name, victims, param_list):
+def set_player_models(world, observer_name, players, victims):
     """
     :param world: the PsychSim World
     :type world: World
     :param observer_name: the name of the agent whose beliefs we will be specifying
     :type observer_name: str
-    :param player_name: the name of the player agent to be modeled
-    :type player_name: str
-    :param param_list: list of dictionaries of model parameter specifications
-    :type param_list: List[Dict]
+    :param players: player names and a list of model parameters
+    :type players: Dict[List] 
     :param victims: specification of victims
     :type victims: Victims
     """
     observer = world.agents[observer_name]
-    player = world.agents[player_name]
 
     # observer does not model itself
-    observer.resetBelief(ignore={modelKey(observer.name)})
+    observer.create_belief_state()
 
-    # get the canonical name of the "true" player model
-    true_model = player.get_true_model()
+    for player_name, param_list in players.items():
+        # get the canonical name of the "true" player model
+        player = world.agents[player_name]
+        true_model = player.get_true_model()
 
-    for param_dict in param_list:
-        model_name = param_dict['name']
-        if model_name != true_model:
-            player.addModel(model_name, parent=true_model,
-                            horizon=param_dict.get('horizon', 2),
-                            rationality=param_dict.get('rationality', 0.5),
-                            selection=param_dict.get('selection', 'distribution'))
-        if isinstance(next(iter(param_dict['reward'].keys())), str):
-            victims.makeVictimReward(player, model_name, param_dict['reward'])
-        else:
-            for feature, weight in param_dict['reward'].items():
-                feature.set_reward(player, weight, model_name)
-        beliefs = player.resetBelief(model=model_name, ignore={modelKey(observer.name)})
+        for param_dict in param_list:
+            model_name = param_dict['name']
+            if model_name != true_model:
+                player.addModel(model_name, parent=true_model,
+                                horizon=param_dict.get('horizon', 2),
+                                rationality=param_dict.get('rationality', 0.5),
+                                selection=param_dict.get('selection', 'distribution'))
+            if isinstance(next(iter(param_dict['reward'].keys())), str):
+                victims.makeVictimReward(player, model_name, param_dict['reward'])
+            else:
+                for feature, weight in param_dict['reward'].items():
+                    feature.set_reward(player, weight, model_name)
+            beliefs = player.resetBelief(model=model_name, ignore={modelKey(observer.name)})
 
-    # observer has uniform prior distribution over possible player models
-    if len(player.models) > 1:
-        world.setMentalModel(observer.name, player.name,
-                             Distribution({param_dict['name']: 1. / (len(player.models) - 1) for param_dict in param_list}))
+        # observer has uniform prior distribution over possible player models
+        if len(player.models) > 1:
+            world.setMentalModel(observer.name, player.name,
+                                 Distribution({param_dict['name']: 1. / (len(player.models) - 1) for param_dict in param_list}))
 
     # observer sees everything except true models
-    observer.omega = [key for key in world.state.keys()
-                      if key not in {modelKey(player.name), modelKey(observer.name)}]  # rewardKey(player.name),
+    observer.set_observations()
