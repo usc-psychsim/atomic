102ac0c8cb2fc5e0f89a16ad09ef511b8edbe65f
Sudeepta Mondal <88106088+MondalSudeepta@users.noreply.github.com>
2021-08-03 15:30:53 -0400

    Update remap_connections.py
    added a section which provides the new connections

--- atomic/parsing/remap_connections.py
+++ atomic/parsing/remap_connections.py
@@ -60,24 +60,24 @@ def transformed_connections(input_map):
       elif  int('part' in input_map["locations"][k]["type"]) == 0 and int('child_locations' in input_map["locations"][k].keys()) == 0 :
           original_dict["original_locations"].append(input_map["locations"][k]['id'])
   
-  original_dict["original_connections"] = [0 for _ in range(len(original_dict["original_locations"]))]
-  original_dict["original_connection_names"] = [[] for _ in range(len(original_dict["original_locations"]))]
+  original_dict["num_neighbors"] = [0 for _ in range(len(original_dict["original_locations"]))]
+  original_dict["original_neighbors"] = [[] for _ in range(len(original_dict["original_locations"]))]
   
   for k in range(len(input_map["connections"])):
       for j in range(len(input_map["connections"][k]["connected_locations"])):
           
-          original_dict["original_connections"][original_dict["original_locations"].index(input_map["connections"][k]["connected_locations"][j])] +=1 
-          original_dict["original_connection_names"][original_dict["original_locations"].index(input_map["connections"][k]["connected_locations"][j])].append(input_map["connections"][k]["connected_locations"][:j]
+          original_dict["num_neighbors"][original_dict["original_locations"].index(input_map["connections"][k]["connected_locations"][j])] +=1 
+          original_dict["original_neighbors"][original_dict["original_locations"].index(input_map["connections"][k]["connected_locations"][j])].append(input_map["connections"][k]["connected_locations"][:j]
                                                                                                                                                    + input_map["connections"][k]["connected_locations"][j+1:]) 
-  for k in range(len(original_dict["original_connection_names"])):
-      original_dict["original_connection_names"][k] = sum(original_dict["original_connection_names"][k], [])
+  for k in range(len(original_dict["original_neighbors"])):
+      original_dict["original_neighbors"][k] = sum(original_dict["original_neighbors"][k], [])
   
   # Creating the new Dictionary of connections
           
   new_dict = {}  
   original_dict["indices_connected"] = [] 
   new_dict["grouped_original_locations"] = []
-  new_dict["connected_locations"] = []  
+  new_dict["neighbors"] = []  
        
   sum_connection = 0
   count = 0
@@ -86,15 +86,15 @@ def transformed_connections(input_map):
   
   for k in range(len(original_dict["original_locations"])):
       count += 1
-      sum_connection += original_dict["original_connections"][k]
+      sum_connection += original_dict["num_neighbors"][k]
   
       if k > 0 and get_parent_room(original_dict["original_locations"][k]) !=  get_parent_room(original_dict["original_locations"][k-1]):
           new_dict["grouped_original_locations"].append(list(original_dict["original_locations"][max(0, k - count) : k]))
           
           lst = list(range(max(0, k - count), k))
           original_dict["indices_connected"].append(lst)
-          new_dict["connected_locations"].append(sorted_list(list(set().union(*original_dict["original_connection_names"][lst[0]:lst[-1] + 1]))))
-          sum_connection = original_dict["original_connections"][k]
+          new_dict["neighbors"].append(sorted_list(list(set().union(*original_dict["original_neighbors"][lst[0]:lst[-1] + 1]))))
+          sum_connection = original_dict["num_neighbors"][k]
           count = 0
       elif sum_connection > conn_thresh and '_' in original_dict["original_locations"][k]:
         if (original_dict["original_locations"][k][0:original_dict["original_locations"][k].index('_')] 
@@ -103,33 +103,33 @@ def transformed_connections(input_map):
           new_dict["grouped_original_locations"].append(list(original_dict["original_locations"][max(0, k - count) : k]))
           lst = list(range(max(0, k - count), k))
           original_dict["indices_connected"].append(lst)
-          new_dict["connected_locations"].append(sorted_list(list(set().union(*original_dict["original_connection_names"][lst[0]:lst[-1] + 1]))))
-          sum_connection = original_dict["original_connections"][k]
+          new_dict["neighbors"].append(sorted_list(list(set().union(*original_dict["original_neighbors"][lst[0]:lst[-1] + 1]))))
+          sum_connection = original_dict["num_neighbors"][k]
           count = 0
   new_dict["grouped_original_locations"].append(list(original_dict["original_locations"][original_dict["indices_connected"][-1][-1] + 1::]))  
-  new_dict["connected_locations"].append(sorted_list(list(set().union(*original_dict["original_connection_names"][original_dict["indices_connected"][-1][-1] + 1:]))))
+  new_dict["neighbors"].append(sorted_list(list(set().union(*original_dict["original_neighbors"][original_dict["indices_connected"][-1][-1] + 1:]))))
   original_dict["indices_connected"].append(list(range(original_dict["indices_connected"][-1][-1] + 1,len(original_dict["original_locations"]))))
   
   # renaming new connections
-  new_dict["new_connection_names"] = [] 
+  new_dict["new_locations"] = [] 
   count = 0
   for k in range(len(new_dict["grouped_original_locations"])):
       if k == 0 and '_' in new_dict["grouped_original_locations"][k][0]:
-        new_dict["new_connection_names"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]) + '_'+chr(65 + count))
+        new_dict["new_locations"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]) + '_'+chr(65 + count))
         count += 1
       elif k ==0 and '_' not in new_dict["grouped_original_locations"][k][0]:
-        new_dict["new_connection_names"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]))
+        new_dict["new_locations"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]))
       elif k > 0 and '_' in new_dict["grouped_original_locations"][k][0] and \
       get_parent_room(new_dict["grouped_original_locations"][k][0]) == get_parent_room(new_dict["grouped_original_locations"][k - 1][0]):
-        new_dict["new_connection_names"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]) + '_'+chr(65 + count))
+        new_dict["new_locations"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]) + '_'+chr(65 + count))
         count += 1
       elif k > 0 and '_' in new_dict["grouped_original_locations"][k][0] and \
       get_parent_room(new_dict["grouped_original_locations"][k][0]) != get_parent_room(new_dict["grouped_original_locations"][k - 1][0]):
         count = 0
-        new_dict["new_connection_names"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]) + '_'+chr(65 + count))
+        new_dict["new_locations"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]) + '_'+chr(65 + count))
         count += 1
       else : 
-        new_dict["new_connection_names"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]))
+        new_dict["new_locations"].append (get_parent_room(new_dict["grouped_original_locations"][k][0]))
         count = 0
   
   # Lookup dictionary for transformation of location names
@@ -137,6 +137,14 @@ def transformed_connections(input_map):
   for orig in original_dict["original_locations"]:
       for j in range(len(new_dict["grouped_original_locations"])):
         if orig in new_dict['grouped_original_locations'][j]:
-          name_transformations[orig] = new_dict['new_connection_names'][j]
+          name_transformations[orig] = new_dict['new_locations'][j]
   
-  return name_transformations, new_dict
+  new_connections = []  
+  for k in range(len(new_dict["new_locations"])): 
+    for j in range((len(new_dict["neighbors"][k]))):
+      if new_dict["new_locations"][k] != name_transformations[new_dict["neighbors"][k][j]] and \
+      [new_dict["new_locations"][k],name_transformations[new_dict["neighbors"][k][j]]] not in new_connections:
+        new_connections.append([new_dict["new_locations"][k],name_transformations[new_dict["neighbors"][k][j]]])
+  
+
+  return  new_connections, name_transformations, new_dict, original_dict
