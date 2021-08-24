96bd581ade0066757505cb9b6fd8a2b294384cac
David V. Pynadath <david@pynadath.net>
2021-08-23 22:05:42 -0700

    Use zero-level models from configuration

--- atomic/inference.py
+++ atomic/inference.py
@@ -5,6 +5,7 @@ to improve task performance
 """
 from psychsim.probability import Distribution
 from psychsim.pwl import modelKey
+from psychsim.action import ActionSet
 
 # Possible player model parameterizations
 DEFAULT_MODELS = {'horizon': {'myopic': 2, 'strategic': 4},
@@ -23,11 +24,55 @@ def make_observer(world, team, name='ATOMIC'):
     :return: the newly created agent, also added to the given World
     :rtype: Agent
     """
-    agent = world.addAgent(name)
+    agent = world.addAgent(name, avoid_beliefs=False)
     return agent
 
+def create_player_models(world, players):
+    models = {}
+    param_names = None
+    zero_models = {player_name: [model_name for model_name in world.agents[player_name].models if model_name[-4:] == 'zero'][0]
+        for player_name in players}
+    null_actions = {}
+    for player_name in players:
+        for action in world.agents[player_name].actions:
+            if action['verb'] == 'noop':
+                null_actions[player_name] = action
+                break
+        else:
+            raise ValueError(f'Unable to find noop for {player_name}')
+    null_models = {player_name: world.agents[player_name].zero_level(null=null_actions[player_name]) for player_name in players}
+    for player_name, param_list in players.items():
+        # get the canonical name of the "true" player model
+        player = world.agents[player_name]
+        zero_model = zero_models[player_name]
+        true_model = player.models[player.get_true_model()]
+        new_models = []
+        for param_dict in param_list:
+            if param_names is None:
+                param_names = sorted(param_dict.keys())
+            param_str = '_'.join([f'{param}{param_dict[param]}' for param in param_names])
+            model_name = f'{player_name}_{param_str}'
+            new_model = player.addModel(model_name, parent=true_model['name'] if param_dict.get('tom', 1) > 0 else zero_model, 
+                selection=param_dict.get('selection', 'distribution'), beliefs=True, static=True)
+            for key, value in param_dict.items():
+                if key == 'tom':
+                    if value == 1:
+                        if param_dict.get('others', 1) == 0:
+                            new_model['models'] = {other_name: null_models[other_name] for other_name in players if other_name != player_name}
+                        else:
+                            new_model['models'] = {other_name: zero_models[other_name] for other_name in players if other_name != player_name}
+                    elif value > 1:
+                        raise ValueError(f'Have not implemented {value}-level ToM yet')
+                elif key != 'selection':
+                    new_model[key] = value
+            new_model['parameters'] = param_dict
+#            if new_model['parent'] != zero_model:
+#                beliefs = player.create_belief_state(model=model_name)
+            new_models.append(new_model)
+        models[player_name] = new_models
+    return models
 
-def set_player_models(world, observer_name, players, victims):
+def set_player_models(world, observer_name, players, victims=None):
     """
     :param world: the PsychSim World
     :type world: World
@@ -42,30 +87,13 @@ def set_player_models(world, observer_name, players, victims):
 
     # observer does not model itself
     observer.create_belief_state()
-
-    for player_name, param_list in players.items():
-        # get the canonical name of the "true" player model
+    models = create_player_models(world, players)
+    for player_name, new_models in models.items():
         player = world.agents[player_name]
-        true_model = player.get_true_model()
-
-        for param_dict in param_list:
-            model_name = param_dict['name']
-            if model_name != true_model:
-                player.addModel(model_name, parent=true_model,
-                                horizon=param_dict.get('horizon', 2),
-                                rationality=param_dict.get('rationality', 0.5),
-                                selection=param_dict.get('selection', 'distribution'))
-            if isinstance(next(iter(param_dict['reward'].keys())), str):
-                victims.makeVictimReward(player, model_name, param_dict['reward'])
-            else:
-                for feature, weight in param_dict['reward'].items():
-                    feature.set_reward(player, weight, model_name)
-            beliefs = player.resetBelief(model=model_name, ignore={modelKey(observer.name)})
-
         # observer has uniform prior distribution over possible player models
         if len(player.models) > 1:
             world.setMentalModel(observer.name, player.name,
-                                 Distribution({param_dict['name']: 1. / (len(player.models) - 1) for param_dict in param_list}))
+                                 Distribution({new_model['name']: 1. / len(new_models) for new_model in new_models}))
 
     # observer sees everything except true models
     observer.set_observations()
