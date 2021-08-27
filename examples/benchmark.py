7a3d957e51b81d7ed88d19af661c5abcfc9af50a
Pedro Sequeira <pedrodbs@gmail.com>
2021-08-27 16:36:01 -0700

    repurposed "benchmark" script from Y1 to test both trajectory generation (for one agent) and PsychSim game log parsing from RDDL file; allows starting from random locations in the environment

--- examples/benchmark.py
+++ examples/benchmark.py
@@ -1,4 +1,3 @@
-import argparse
 import json
 import os
 import logging
@@ -6,21 +5,19 @@ import numpy as np
 import multiprocessing as mp
 from collections import OrderedDict
 from timeit import default_timer as timer
-from atomic.definitions.map_utils import get_default_maps
-from atomic.model_learning.linear.rewards import create_reward_vector
-from atomic.model_learning.parse_processor import TrajectoryParseProcessor
-from atomic.parsing.replayer import Replayer, SUBJECT_ID_TAG, COND_MAP_TAG
-from atomic.scenarios.single_player import make_single_player_world
-from model_learning.trajectory import generate_trajectories
-from model_learning.util.cmd_line import none_or_int
-from model_learning.util.plot import plot_bar
-from model_learning.util.io import get_files_with_extension, create_clear_dir, change_log_handler
+from psychsim.pwl import stateKey
+from atomic.parsing.replayer import Replayer, COND_MAP_TAG, TEAM_ID_TAG, replay_parser, parse_replay_args
+from atomic.util.io import create_clear_dir, change_log_handler
+from atomic.util.plot import plot_bar
+from rddl2psychsim.conversion.converter import Converter
+from model_learning.trajectory import generate_trajectories, copy_world
 
 __author__ = 'Pedro Sequeira'
 __email__ = 'pedrodbs@gmail.com'
-__desc__ = 'Simple test script that loads several player log files and creates a plot with the corresponding ' \
-           'imported trajectories\' length.'
+__desc__ = 'Simple test script that loads several team log files and creates a plot with the corresponding ' \
+           'imported trajectories\' length, processing times, etc.'
 
+LOG_EXTENSION = 'metadata'
 OUTPUT_DIR = 'output/benchmark'
 
 # params
@@ -32,11 +29,7 @@ PRUNE_THRESHOLD = 5e-2  # 1e-2
 KEEP_K = 10
 HORIZON = 2
 SEED = 0
-PROCESSES = None
-
-MAP_NAME = 'FalconEasy'
-PLAYER_NAME = 'Player'
-FULL_OBS = True
+PROCESSES = -1
 
 REWARD_WEIGHTS = np.array([
     0,  # Before Mid
@@ -59,37 +52,134 @@ def _signal_traj_completion():
 
 class BenchmarkReplayer(Replayer):
 
-    def __init__(self, replays, maps=None):
-        super().__init__(replays, maps, {}, create_observer=False, processor=TrajectoryParseProcessor())
-
+    def __init__(self):
+        super().__init__(args['fname'], rddl_file=args['rddl'], action_file=args['actions'], aux_file=args['aux'])
+        # per file stats
         self.timings = {}
-        self.subject_ids = {}
+        self.ids = {}
         self.trajectories = {}
+        self.total_lengths = {}
+
+        # current file stats
+        self.trajectory = []
+        self.step_times = []
+        self.prev_world = None
+        self.start = -1.
+
+    def pre_step(self, logger=logging):
+        self.prev_world = copy_world(self.world)  # todo change agent world cloning
+        self.start = timer()
+
+    def post_step(self, actions, debug, logger=logging):
+        elapsed = timer() - self.start
+        self.logger.info(f'Step {len(self.trajectory)}: {elapsed:.3f}s')
+        self.step_times.append(elapsed)
+        self.trajectory.append((self.prev_world, actions))
 
     def replay(self, duration, logger):
+        # reset
+        self.trajectory = []
+        self.step_times = []
+        self.prev_world = None
+        self.start = 0.
         start = timer()
-        super(BenchmarkReplayer, self).replay(duration, logger)
+        try:
+            exception = None
+            super(BenchmarkReplayer, self).replay(duration, logger)
+        except Exception as e:
+            exception = e
+
+        # process results
         elapsed = timer() - start
-        self.logger.info('Parsed {} in {:.3f}s'.format(self.parser.filename, elapsed))
-        self.subject_ids[self.parser.filename] = \
-            '{}-{}'.format(self.conditions[SUBJECT_ID_TAG], self.conditions[COND_MAP_TAG][0]) \
-                if SUBJECT_ID_TAG in self.conditions and COND_MAP_TAG in self.conditions else \
-                self.parser.player_name()
+        self.logger.info(f'Parsed {self.parser.filename} in {elapsed:.3f}s')
+        self.ids[self.parser.filename] = \
+            '{}-{}'.format(self.conditions[TEAM_ID_TAG], self.conditions[COND_MAP_TAG][0]) \
+                if TEAM_ID_TAG in self.conditions and COND_MAP_TAG in self.conditions else \
+                os.path.basename(self.parser.filename)
         self.timings[self.parser.filename] = elapsed
-        self.trajectories[self.parser.filename] = self.processor.trajectory
+        self.trajectories[self.parser.filename] = self.trajectory
+        self.total_lengths[self.parser.filename] = len(self.parser.actions)
+
+        if exception is not None:
+            raise exception
+
+
+def _generate_trajectories():
+    # create world & agents from RDDL
+    rddl_converter = Converter()
+    rddl_converter.convert_file(rddl_file=args['rddl'])
+    world = rddl_converter.world
+
+    # set params to the first agent, other's decide at random
+    agent = next(iter(world.agents.values()))
+    for ag in world.agents.values():
+        if ag != agent:
+            ag.setAttribute('selection', 'random')
+            ag.setAttribute('horizon', 0)
+    agent.setAttribute('rationality', args['rationality'])
+    agent.setAttribute('selection', args['selection'])
+    agent.setAttribute('horizon', args['horizon'])
+
+    # TODO set agent rwd function
+    # rwd_vector = create_reward_vector(agent, map_table.rooms_list, world_map.moveActions[agent.name])
+    # rwd_vector.set_rewards(agent, REWARD_WEIGHTS)
+    # logging.info('Set reward vector: {}'.format(dict(zip(rwd_vector.names, REWARD_WEIGHTS))))
+
+    # randomize initial location
+    features = [stateKey(agent.name, 'pLoc')]
+
+    # generate trajectories
+    n_procs = args['processes'] if args['processes'] > 0 else mp.cpu_count()
+    logging.info(f'Generating {args["trajectories"]} trajectories of length {args["length"]} '
+                 f'using {n_procs} parallel processes...')
+    start = timer()
+    trajectories = generate_trajectories(
+        agent, args['trajectories'], args['length'],
+        features=features,
+        select=None,
+        threshold=args['prune'],
+        processes=n_procs,
+        seed=args['seed'],
+        verbose=True)
+    elapsed = (timer() - start) * n_procs
+
+    logging.info('(mean: {:.3f}s per trajectory, {:.3f}s per step)'.format(
+        elapsed / args['trajectories'], elapsed / (args['trajectories'] * args['length'])))
+
+
+def _process_files():
+    replayer.process_files()
+
+    files = sorted(replayer.trajectories.keys())
+    lengths = np.array([len(replayer.trajectories[filename]) for filename in files])
+    times = np.array([replayer.timings[filename] for filename in files])
+    logging.info('Parsing of {} files took a total of {:.3f}s (mean: {:.3f}s per file, {:.3f}s per step)'.format(
+        len(replayer.timings), np.sum(times), np.mean(times), np.mean(times / lengths)))
+
+    # prints results
+    files = sorted(replayer.timings.keys())
+    times = [replayer.timings[filename] for filename in files]
+    ids = [replayer.ids[filename] for filename in files]
+    perc_parsed = ((lengths / np.array([replayer.total_lengths[filename] for filename in files])) * 100).astype(int)
+    plot_bar(OrderedDict(zip(ids, times)), 'Processing Times', os.path.join(args['output'], 'process-times.pdf'))
+    plot_bar(OrderedDict(zip(ids, lengths)), 'Parsed Lengths', os.path.join(args['output'], 'parse-lengths.pdf'))
+    plot_bar(OrderedDict(zip(ids, lengths)), 'Parsed Percentage', os.path.join(args['output'], 'parse-percent.pdf'))
+    overall_stats = {'Processing Times': (np.mean(times), np.std(times)),
+                     'Parsed Lengths': (np.mean(lengths), np.std(lengths)),
+                     'Parsed Percentage': (np.mean(perc_parsed), np.std(perc_parsed))}
+    plot_bar(overall_stats, 'Average Stats', os.path.join(args['output'], 'overall-stats.pdf'), plot_mean=False)
 
 
 if __name__ == '__main__':
     # parse command-line arguments
-    parser = argparse.ArgumentParser(description=__desc__)
+    parser = replay_parser()
+    parser.description = __desc__
 
     parser.add_argument('-o', '--output', type=str, default=OUTPUT_DIR, help='Directory in which to save results.')
-    parser.add_argument('-r', '--replays', type=str, default=None,
-                        help='Directory containing the replay logs or single replay file to process.')
     parser.add_argument('-t', '--trajectories', type=int, default=NUM_TRAJECTORIES,
-                        help='Number of trajectories to use in reward learning.')
+                        help='Number of trajectories to generate in benchmarking.')
     parser.add_argument('-l', '--length', type=int, default=TRAJ_LENGTH,
-                        help='Length of trajectories used in reward learning.')
+                        help='Length of trajectories used to generate in benchmarking.')
     parser.add_argument('--horizon', type=int, default=HORIZON, help='Planning horizon of the agent.')
     parser.add_argument('--rationality', type=float, default=RATIONALITY, help='Agent\'s rationality.')
     parser.add_argument('--selection', type=str, default=SELECTION, help='Agent\'s selection policy.')
@@ -97,83 +187,28 @@ if __name__ == '__main__':
                         help='Likelihood below which stochastic outcomes are pruned.')
 
     parser.add_argument('-s', '--seed', type=int, default=SEED, help='Seed for random number generation.')
-    parser.add_argument('-m', '--map-name', type=str, default=MAP_NAME,
-                        help='Name of the map for trajectory generation.')
-    parser.add_argument('-p', '--processes', type=none_or_int, default=PROCESSES,
-                        help='Number of processes/cores to use. If unspecified, all available cores will be used')
-    args = parser.parse_args()
+    parser.add_argument('-p', '--processes', type=int, default=PROCESSES,
+                        help='Number of processes/cores to use. If unspecified, all available cores will be used.')
+
+    args = parse_replay_args(parser)
 
     # create output and log file
-    create_clear_dir(args.output, False)
-    change_log_handler(os.path.join(args.output, 'learning.log'))
+    create_clear_dir(args['output'], False)
+    change_log_handler(os.path.join(args['output'], 'learning.log'), verbosity=args['debug'])
 
     # saves args
-    with open(os.path.join(args.output, 'args.json'), 'w') as fp:
-        json.dump(vars(args), fp, indent=4)
-
-    # checks input files
-    files = []
-    if args.replays is None:
-        logging.info('No replay file provided, skipping parsing benchmark.'.format(args.replays))
-    elif os.path.isfile(args.replays):
-        files = [args.replays]
-    elif os.path.isdir(args.replays):
-        files = list(get_files_with_extension(args.replays, 'csv'))
-    else:
-        logging.info('Provided replay path is not a valid file or directory: {}.'.format(args.replays))
+    with open(os.path.join(args['output'], 'args.json'), 'w') as fp:
+        json.dump(args, fp, indent=4)
 
     # create replayer and process all files
-    if len(files) > 0:
-        replayer = BenchmarkReplayer(files)
-        replayer.process_files()
-
-        files = sorted(replayer.trajectories.keys())
-        lengths = np.array([len(replayer.trajectories[filename]) for filename in files])
-        times = np.array([replayer.timings[filename] for filename in files])
-        logging.info('Parsing of {} files took a total of {:.3f}s (mean: {:.3f}s per file, {:.3f}s per step)'.format(
-            len(replayer.timings), np.sum(times), np.mean(times), np.mean(times / lengths)))
-
-        # prints results
-        files = sorted(replayer.timings.keys())
-        times = [replayer.timings[filename] for filename in files]
-        subject_ids = [replayer.subject_ids[filename] for filename in files]
-        plot_bar(OrderedDict(zip(subject_ids, times)), 'Parsing Times', os.path.join(args.output, 'parse-times.pdf'))
-        plot_bar(OrderedDict(zip(subject_ids, lengths)), 'Trajectory Lengths',
-                 os.path.join(args.output, 'parse-lengths.pdf'))
+    replayer = BenchmarkReplayer()
+    if len(replayer.files) == 0:
+        logging.info('Skipping game logs processing benchmark.')
+    else:
+        _process_files()
 
     # generate trajectories
-    default_maps = get_default_maps()
-    if args.trajectories == 0 or args.map_name not in default_maps:
-        msg = 'Skipping generation benchmark. '
-        if args.map_name not in default_maps:
-            msg += 'Map name {} not in default maps.'.format(args.map_name)
-        logging.info(msg)
-
+    if args['trajectories'] == 0:
+        logging.info('Skipping trajectory generation benchmark.')
     else:
-        # create world, agent and observer
-        map_table = default_maps[args.map_name]
-        world, agent, observer, victims, world_map = make_single_player_world(
-            PLAYER_NAME, map_table.init_loc, map_table.adjacency, map_table.victims,
-            False, FULL_OBS, False)
-
-        # agent params
-        agent.setAttribute('rationality', args.rationality)
-        agent.setAttribute('selection', args.selection)
-        agent.setAttribute('horizon', args.horizon)
-
-        # set agent rwd function
-        rwd_vector = create_reward_vector(agent, map_table.rooms_list, world_map.moveActions[agent.name])
-        rwd_vector.set_rewards(agent, REWARD_WEIGHTS)
-        logging.info('Set reward vector: {}'.format(dict(zip(rwd_vector.names, REWARD_WEIGHTS))))
-
-        # generate trajectories
-        logging.info('Generating {} trajectories of length {} using {} parallel processes...'.format(
-            args.trajectories, args.length, args.processes if args.processes is not None else mp.cpu_count()))
-        start = timer()
-        trajectories = generate_trajectories(
-            agent, args.trajectories, args.length, threshold=args.prune, processes=args.processes, seed=args.seed,
-            verbose=True)
-
-        elapsed = timer() - start
-        logging.info('(mean: {:.3f}s per trajectory, {:.3f}s per step)'.format(
-            elapsed / args.trajectories, elapsed / (args.trajectories * args.length)))
+        _generate_trajectories()
