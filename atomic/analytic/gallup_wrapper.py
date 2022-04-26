0d80c488b44a7bb5f55a14b4375e363af92a816e
David V. Pynadath <david@pynadath.net>
2022-04-25 23:12:15 -0700

    Reworked GELP and added placeholder GOLD

--- atomic/analytic/gallup_wrapper.py
+++ atomic/analytic/gallup_wrapper.py
@@ -7,6 +7,7 @@ Created on Tue Mar 15 13:32:51 2022
 """
 
 from .acwrapper import ACWrapper
+import json
 import pandas as pd
 import numpy as np
 
@@ -19,26 +20,33 @@ class GelpWrapper(ACWrapper):
             'trial': self.handle_trial,
             'agent/gelp': self.handle_msg}
 
-        self.make_dfs()
+        self.data = pd.DataFrame()
         
     def handle_msg(self, message, data):
-        if message['timestamp'] is None:
-            return
-        elapsed = [self.elapsed_millis(message)]
-        comp_results = {r['callsign']:r['gelp_components'] for r in data['gelp_results']}
-        for gcomponent in range(len(self.score_names)-1):
-            row = [comp_results.get(callsign, np.zeros(self.n_scores()))[gcomponent] for callsign in self.callsigns]
-            self.data[gcomponent].loc[len(self.data[gcomponent])] = elapsed + row
-            
-        
-        overall_results = {r['callsign']:r['gelp_overall'] for r in data['gelp_results']}
-        self.data[-1].loc[len(self.data[-1])] = [self.elapsed_millis(message)] + \
-                                        [overall_results.get(callsign, 0) for callsign in self.callsigns]
-                                        
-        
+        new_data = []
+        if message['timestamp'] is not None:
+            for result in data['gelp_results']:
+                record = {self.score_names[i]: value for i, value in enumerate(result['gelp_components'])}
+                record['Player'] = result['callsign']
+                record[self.score_names[-1]] = result['gelp_overall']
+                new_data.append(record)
+            self.last = pd.DataFrame(new_data)
+            self.last['Timestamp'] = message['timestamp']
+            self.data = pd.concat([self.data, self.last], ignore_index=True)
+            # elapsed = [self.elapsed_millis(message)]
+            # comp_results = {r['callsign']:r['gelp_components'] for r in data['gelp_results']}
+            # for gcomponent in range(len(self.score_names)-1):
+            #     row = [comp_results.get(callsign, np.zeros(self.n_scores()))[gcomponent] for callsign in self.callsigns]
+            #     self.data[gcomponent].loc[len(self.data[gcomponent])] = elapsed + row
+                
+            # overall_results = {r['callsign']:r['gelp_overall'] for r in data['gelp_results']}
+            # self.data[-1].loc[len(self.data[-1])] = [self.elapsed_millis(message)] + \
+            #                                        [overall_results.get(callsign, 0) for callsign in self.callsigns]
+
         # print(len(self.messages))
         # if (len(self.messages) % 10) == 1:
         #     print(self.name, self.compare(10))
+        return new_data
     
     '''
 “Overall” leadership score: mean = 6.1, SD = 1.6. 
@@ -52,4 +60,16 @@ PLAN: mean = 4.1, SD = 1.3.
 AGREE: mean = 4.6, SD = 1.2. 
 HELP: mean = 4.8, SD = 1.2.
 '''
-                    
+          
+
+class GOLDWrapper(ACWrapper):
+    def __init__(self, team_name, ac_name):
+        super().__init__(team_name, ac_name)
+        self.topic_handlers = {
+            'trial': self.handle_trial,
+            'agent/gold': self.handle_msg}
+
+        self.data = pd.DataFrame()
+
+    def handle_msg(self, message, data):
+        print(data.keys())
