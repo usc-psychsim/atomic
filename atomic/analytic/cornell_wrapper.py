7342338fd0c4fab206d93e7c1a361cf723e10804
David V. Pynadath <david@pynadath.net>
2022-04-26 22:56:08 -0700

    Handle goal alignment

--- atomic/analytic/cornell_wrapper.py
+++ atomic/analytic/cornell_wrapper.py
@@ -18,10 +18,11 @@ class ComplianceWrapper(ACWrapper):
         self.score_names = ['avg_response_time', 'N_requests_open', 'compliance_ratio']
         self.topic_handlers = {
             'trial': self.handle_trial,
-            'agent/ac/player_compliance': self.handle_msg}
+            'agent/ac/player_compliance': self.handle_compliance_msg,
+            'agent/ac/goal_alignment': self.handle_alignment_msg}
         self.data = pd.DataFrame()
 
-    def handle_msg(self, message, data):
+    def handle_compliance_msg(self, message, data):
         new_data = []
         for player1, table in data.items():
             if player1 != 'elapsed_ms':
@@ -43,4 +44,22 @@ class ComplianceWrapper(ACWrapper):
         #    row = elapsed + [data.get(score+'_'+callsign, 0) for callsign in self.callsigns]
         #    self.data[si].loc[len(self.data[si])] = row
         return new_data
-            
+
+    def handle_alignment_msg(self, message, data):
+        new_data = []
+        for player1, table in data.items():
+            if player1 == 'Team':
+                new_data.append(table)
+                new_data[-1]['Requestor'] = player1
+                new_data[-1]['goal_alignment_current'] = 1 if table['goal_alignment_current'] else 0
+            elif player1 != 'elapsed_ms':
+                for player2 in table['goal_alignment_current']:
+                    record = {'Requestor': f'{player1.upper()}_ASIST2', 'Requestee': f'{player2.upper()}_ASIST2',
+                              'current_goal': table['current_goal']}
+                    record.update({field: value[player2] for field, value in table.items() if field != 'current_goal'})
+                    record['goal_alignment_current'] = 1 if record['goal_alignment_current'] else 0
+                    new_data.append(record)
+        self.last = pd.DataFrame(new_data)
+        self.last['Timestamp'] = message['timestamp']
+        self.data = pd.concat([self.data, self.last], ignore_index=True)
+        return new_data            
