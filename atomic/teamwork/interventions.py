05314f72ebf973674cfa22fee94b58d1f95bffbc
David V. Pynadath <david@pynadath.net>
2022-05-04 15:47:08 -0700

    Made interventions all team-level. Added legality conditions. Added 'valid on start' flag

--- atomic/teamwork/interventions.py
+++ atomic/teamwork/interventions.py
@@ -1,20 +1,21 @@
-from psychsim.pwl.keys import stateKey,WORLD
+from psychsim.pwl.keys import stateKey, WORLD
 from psychsim.pwl.plane import thresholdRow
 
 interventions = {  # Inter-mission AAR prompt
                    # Associated state: Descriptors from situation to highlight (implies descriptors of current situation are being maintained)
-                   'reflect': {'object': 'player',
+                   'reflect': {'object': 'team',
                                'template': 'I\'ve noticed an instance when ${object} was waiting for longer than usual. Do you remember ${situation}? Could you comment on a) what you think happened, b) how you could identify this the next time, c) what to do when you identify this occuring?',
                                'effects': {'coordination': 1},
                                # Legal only before time 0
-                               'legal': {'if': thresholdRow(stateKey(WORLD, 'clock'), 0), True: False, False: True}},
+                               'legal': {'if': thresholdRow(stateKey(WORLD, 'clock'), 0), True: False, False: True},
+                               'valid on start': True},
                    # Cheerleading action
-                   'cheer': {'object': 'player',
-                             'template': 'Great job, ${object}!',
+                   'cheer': {'object': 'team',
+                             'template': 'Great job getting that victim into the triage area, ${Player}!',
                              'effects': {'motivating': 1, 'affect management': 1, 'cognitive load': 0.2}},
                    # Report performance change
                    # Associated state: Individual performance level, team leader
-                   'report drop': {'object': 'player',
+                   'report drop': {'object': 'team',
                                    'template': 'Can you check on ${object}? They haven\'t been responding.',
                                    'effects': {'team monitoring': 1, 'cognitive load': 0.2}},
                    # Recommend phase-sensitive plan (early)
@@ -23,7 +24,8 @@ interventions = {  # Inter-mission AAR prompt
                                           'template': 'Team, it looks like you\'re working well to clear this section, however the building is large and spreading out early on may be useful.',
                                           'effects': {'systems monitoring': 1, 'cognitive load': 0.2},
                                           'legal': {'if': thresholdRow(stateKey(WORLD, 'clock'), [300, 360]),
-                                                    0: False, 1: True, 2: False}
+                                                    0: False, 1: True, 2: False},
+                                          'valid on start': True
                                           },
                    # Recommend phase-sensitive plan (late)
                    # Associated state: Game phase
@@ -31,16 +33,17 @@ interventions = {  # Inter-mission AAR prompt
                                          'template': 'Team, the mission is nearing its end, it\'s time to work together in a high-value region and focus on transport and triaging to get points!',
                                          'effects': {'systems monitoring': 1, 'cognitive load': 0.2},
                                          'legal': {'if': thresholdRow(stateKey(WORLD, 'clock'), [600, 660]),
-                                                   0: False, 1: True, 2: False}
+                                                   0: False, 1: True, 2: False},
+                                         'valid on start': True
                                          },
                    # Prompt for coordination best practices
                    # Associated state: Unassigned requests/goals
-                   'remind practices': {'object': 'player',
+                   'remind practices': {'object': 'team',
                                         'template': 'No one has fulfilled ${object}\'s request. Can anyone help?',
                                         'effects': {'coordination': 1, 'cognitive load': 0.2}},
                    # Spread workload
                    # Associated state: workload of individual players
-                   'distribute workload': {'object': 'player',
+                   'distribute workload': {'object': 'team',
                                            'template': 'Can someone help ${object} who is currently overloaded?',
                                            'effects': {'team monitoring': 1, 'coordination': 1, 'cognitive load': 0.2}},
                  }
