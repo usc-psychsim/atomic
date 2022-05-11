e76f2de629e4f533c911f8c24c1a998a49b54633
David V. Pynadath <david@pynadath.net>
2022-05-10 23:37:15 -0700

    Modify messages over multiple trials with same team

--- atomic/teamwork/interventions.py
+++ atomic/teamwork/interventions.py
@@ -5,7 +5,7 @@ interventions = {  # Inter-mission AAR prompt
                    # Associated state: Descriptors from situation to highlight (implies descriptors of current situation are being maintained)
                    'reflect': {'object': 'team',
                                'template': ['Good luck, team!',
-                                            'Back at the ${time_minutes}-minute mark, ${Player} had to wait more than ${threshold} seconds while frozen in ${threat_room}. Could you comment on a) what you think happened, b) how you could identify this the next time, c) what to do when you identify this occuring?'],
+                                            'Back at the ${time_minutes}-minute mark, ${Player} had to wait ${wait_time} seconds while frozen in ${threat_room}. Could you comment on a) what you think happened, b) how you could identify this the next time, and c) what to do when you identify this occuring?'],
                                'effects': {'coordination': 1},
                                # Legal only before time 0
                                'legal': {'if': thresholdRow(stateKey(WORLD, 'clock'), 0), True: False, False: True},
@@ -22,7 +22,8 @@ interventions = {  # Inter-mission AAR prompt
                    # Recommend phase-sensitive plan (early)
                    # Associated state: Game phase
                    'notify early phase': {'object': 'team',
-                                          'template': 'Team, it looks like you\'re working well to clear this section; however, the building is large, so spreading out may be more useful now.',
+                                          'template': ['Team, it looks like you\'re working well to clear this section; however, the building is large, so spreading out may be more useful now.',
+                                                       'Team, remember to focus on exploring now.'],
                                           'effects': {'systems monitoring': 1, 'cognitive load': 1},
                                           'legal': {'if': thresholdRow(stateKey(WORLD, 'clock'), [300, 360]),
                                                     0: False, 1: True, 2: False},
@@ -31,7 +32,8 @@ interventions = {  # Inter-mission AAR prompt
                    # Recommend phase-sensitive plan (late)
                    # Associated state: Game phase
                    'notify late phase': {'object': 'team',
-                                         'template': 'Team, the mission is nearing its end, it\'s time to work together in a more promising region and worry less about exploration!',
+                                         'template': ['Team, the mission is nearing its end, it\'s time to work together in a more promising region and worry less about exploration!',
+                                                      'Team, time to worry less about exploring!'],
                                          'effects': {'systems monitoring': 1, 'cognitive load': 1},
                                          'legal': {'if': thresholdRow(stateKey(WORLD, 'clock'), [600, 660]),
                                                    0: False, 1: True, 2: False},
@@ -48,3 +50,5 @@ interventions = {  # Inter-mission AAR prompt
                                        'template': 'There\'s not much activity. Is anyone stuck?',
                                        'effects': {'team monitoring': 1, 'coordination': 1, 'cognitive load': 1}},
                  }
+
+intervention_patches = [[]]
