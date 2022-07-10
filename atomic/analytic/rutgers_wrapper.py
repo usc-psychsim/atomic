c3126ec183ebcae003ae30a7e0871285b5d5dd47
David V. Pynadath <david@pynadath.net>
2022-07-09 23:47:33 -0500

    Logging of Rutgers variables

--- atomic/analytic/rutgers_wrapper.py
+++ atomic/analytic/rutgers_wrapper.py
@@ -10,19 +10,27 @@ class BeliefDiffWrapper(ACWrapper):
         self.topic_handlers = {
             'trial': self.handle_trial,
             'agent/ac/belief_diff': self.handle_msg,
-            'agent/ac/threat_room_communication': self.ignore_msg,
-            'agent/ac/victim_type_communication': self.ignore_msg,
-            'agent/ac/threat_room_coordination': self.handle_threat}
+            'agent/ac/threat_room_communication': self.handle_msg,  # self.ignore_msg,
+            'agent/ac/victim_type_communication': self.handle_msg,  # self.ignore_msg,
+            'agent/ac/threat_room_coordination': self.handle_msg,  # self.handle_threat
+            }
         self.data = pd.DataFrame()
 
     def handle_msg(self, message, data, mission_time):
-        overall_pos = data['room_id'].index('overall')
-        new_data = {'Timestamp': mission_time}
+        new_data = self.world.make_record()
         for field, value in data.items():
-            if isinstance(value, list) and field != 'room_id':
-                new_data[field] = value[overall_pos]
+            if isinstance(value, list):
+                new_data[field] = value[-1]
+            else:
+                new_data[field] = value
+        if 'threat_activation_player' in data:
+            new_data['Player'] = new_data['threat_activation_player'].split('_')[0].capitalize()
+        # overall_pos = data['room_id'].index('overall')
+        # new_data = self.world.make_record()
+        # for field, value in data.items():
+        #     if isinstance(value, list) and field != 'room_id':
+        #         new_data[field] = value[overall_pos]
         self.last = pd.DataFrame([new_data])
-        self.last['Trial'] = self.trial
         self.data = pd.concat([self.data, self.last], ignore_index=True)
         return [new_data]
 
@@ -30,13 +38,11 @@ class BeliefDiffWrapper(ACWrapper):
         new_data = []
         if data['wait_time'][-1] != float('Infinity'):
             # For now, wait until the threat has been resolved before storing data
-            new_data.append({'Player': data['threat_activation_player'][-1].split('_')[0].capitalize(), 
-                             'Timestamp': mission_time,
-                             'wait_time': int(data['wait_time'][-1]), 
-                             'threat_activation_time': data['threat_activation_time'][-1],
-                             'threat_room': data['room_id'][-1],
-                             'threshold': data.get('threshold', data['threshold:'])})
+            new_data.append(self.world.make_record({'Player': data['threat_activation_player'][-1].split('_')[0].capitalize(), 
+                                                    'wait_time': int(data['wait_time'][-1]), 
+                                                    'threat_activation_time': data['threat_activation_time'][-1],
+                                                    'threat_room': data['room_id'][-1],
+                                                    'threshold': data.get('threshold', data['threshold:'])}))
             self.last = pd.DataFrame(new_data)
-            self.last['Trial'] = self.trial
             self.data = pd.concat([self.data, self.last], ignore_index=True)
             return new_data
