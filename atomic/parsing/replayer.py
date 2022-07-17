7d5f4555ded4f2bb020980b5d02cd6de1130afc7
David V. Pynadath <david@pynadath.net>
2022-07-17 01:05:33 -0700

    Lots of arguments to control output

--- atomic/parsing/replayer.py
+++ atomic/parsing/replayer.py
@@ -3,6 +3,7 @@ import configparser
 import cProfile
 import json
 import logging
+import numpy
 import os.path
 import pandas
 import glob
@@ -14,7 +15,10 @@ except ImportError:
     pbar_manager = None
 import matplotlib.pyplot as plt    
 
+from atomic.analytic import AC_specs
 from atomic.parsing.asist_world import ASISTWorld
+from atomic.parsing.transcript import Transcript
+
 
 COND_MAP_TAG = 'CondWin'
 COND_TRAIN_TAG = 'CondBtwn'
@@ -23,7 +27,7 @@ TEAM_ID_TAG = 'Team'
 TRIAL_TAG = 'Trial'
 
 
-def accumulate_files(files, include_trials=None, ext='.metadata', logger=logging, group_by_team=False):
+def accumulate_files(files, include_trials=None, ext='.metadata', logger=logging, group_by_team=False, clean=False):
     """
     Accumulate a list of files from a given list of names of files and directories
     :type files: List(str)
@@ -57,8 +61,12 @@ def accumulate_files(files, include_trials=None, ext='.metadata', logger=logging
                 continue
         if trial == 'Competency':
             logging.warning(f'Ignoring competency trial {os.path.basename(fname)}')
+            if clean:
+                os.remove(fname)
         elif trial == 'Training':
             logging.warning(f'Ignoring training trial {os.path.basename(fname)}')
+            if clean:
+                os.remove(fname)
         elif include_trials is None or int(trial[1:]) in include_trials:
             trials[trial] = trials.get(trial, []) + [fname]
             teams[conditions['Team']] = teams.get(conditions['Team'], set()) | {trial}
@@ -68,6 +76,9 @@ def accumulate_files(files, include_trials=None, ext='.metadata', logger=logging
             logging.warning(f'Ignoring version(s) of trial {os.path.basename(trial)} '
                             f'{", ".join([filename_to_condition(os.path.splitext(fname)[0])["Vers"] for fname in files[:-1]])} '
                             f'in favor of version {filename_to_condition(os.path.splitext(files[-1])[0])["Vers"]}')
+            if clean:
+                for fname in files[:-1]:
+                    os.remove(fname)
     if group_by_team:
         # Group newest trials by team
         teams = {team: [trials[trial][-1] for trial in sorted(trial_list)] for team, trial_list in teams.items()}
@@ -84,9 +95,9 @@ class Replayer(object):
     :type files: List(str)
     """
 
-    def __init__(self, files=[], trials=None, config=None, strict=False, logger=logging):
+    def __init__(self, files=[], trials=None, config=None, strict=False, clean=False, logger=logging):
         # Extract files to process
-        self.files = accumulate_files(files, trials, group_by_team=True)
+        self.files = accumulate_files(files, trials, group_by_team=True, clean=clean)
 
         if isinstance(config, str):
             self.config = configparser.ConfigParser()
@@ -105,6 +116,10 @@ class Replayer(object):
         self.AC_last = {}
 
         self.log_data = pandas.DataFrame()
+        self.log_columns = None
+        self.ac_columns = set()
+        self.compliance_data = pandas.DataFrame()
+        self.transcripts = {}
 
         self.pbar = None
 
@@ -125,11 +140,15 @@ class Replayer(object):
         self.finish()
 
     def process_file(self, fname, num_steps):
-        self.logger.info(f'Processing file {fname}')
-
         if not isinstance(fname, list):
             fname = [fname]
         for f_index, f in enumerate(fname):
+            self.logger.info(f'Processing file {f}')
+            # if f_index == 0:
+            #     transcript = get_transcript_name(f)
+            #     if transcript is not None:
+            #         trans_obj = Transcript(transcript)
+            #         self.transcripts[filename_to_condition(f)['Team']] = trans_obj
             if f_index == 0:
                 self.pre_replay(fname[0])
             else:
@@ -142,6 +161,7 @@ class Replayer(object):
         self.worlds[fname] = ASISTWorld(config=self.config, logger=logger_child)
             
     def replay(self, fname, duration):
+        conditions = filename_to_condition(fname)
         global pbar_manager
         num_lines = sum(1 for i in open(fname, 'rb'))
         if pbar_manager:
@@ -168,6 +188,8 @@ class Replayer(object):
                         msg = None
                 if msg:
                     self.sources.add(msg['msg']['source'])
+                    if conditions['Team'] in self.transcripts:
+                        self.transcripts[conditions['Team']].insert(msg)
                     try:
                         self.worlds[fname].process_msg(msg)
                     except Exception:
@@ -190,6 +212,14 @@ class Replayer(object):
                     self.AC_last[AC.name] = AC.last
 
         self.log_data = pandas.concat([self.log_data, self.worlds[fname].log_data], ignore_index=True)
+        if self.log_columns is None:
+            self.log_columns = self.worlds[fname].log_columns
+        self.ac_columns |= self.worlds[fname].ac_columns
+        if self.worlds[fname].compliance_data is not None:
+            self.compliance_data = pandas.concat([self.compliance_data, self.worlds[fname].compliance_data],
+                                                 ignore_index=True)
+        if self.config.getboolean('output', 'psychsim', fallback=False):
+            self.worlds[fname].save(os.path.splitext(fname)[0])
         self.worlds[fname].close()
 
     def pre_step(self, world):
@@ -199,52 +229,42 @@ class Replayer(object):
         pass
 
     def finish(self):
-        self.log_data.to_csv('/home/david/Downloads/interventions.csv', index=False)
-        result = pandas.DataFrame()
-        plot_data = {}
-        non_stats = ['Requestor', 'Requestee', 'Timestamp', 'role', 'millis', 
-                     'Player', 'elapsed_ms', 'delta_ms', 'Trial']
-        for name, data in sorted(self.AC_data.items(), key=lambda tup: tup[0].lower()):
-            if data is not None:
-                numeric = data.drop(columns=non_stats, errors='ignore')
-                frame = numeric.min().to_frame('min')
-                frame = frame.join(numeric.max().to_frame('max'))
-                frame = frame.join(numeric.mean().to_frame('mean'))
-                frame = frame.join(numeric.std().to_frame('std'))
-                numeric = self.AC_last[name].drop(columns=non_stats, errors='ignore')
-                frame = frame.join(numeric.min().to_frame('min final'))
-                frame = frame.join(numeric.max().to_frame('max final'))
-                frame = frame.join(numeric.mean().to_frame('mean final'))
-                frame = frame.join(numeric.std().to_frame('std final'))
-                frame.insert(0, 'AC', name)
-                result = pandas.concat([result, frame.sort_index()])
-                # Generate data for graphs
-                subset = ['Timestamp', 'Trial']
-                if 'Player' in data.columns:
-                    subset.append('Player')
-                for y in set(data.columns) - set(non_stats):
-                    y_data = data[subset+[y]]
-                    plot = False
-                    continue
-                    for trial in y_data['Trial'].unique():
-                        trial_data = y_data[y_data['Trial'] == trial]
-                        if 'Player' in trial_data.columns:
-                            for player in trial_data['Player'].unique():
-                                player_data = trial_data[trial_data['Player'] == player]
-                                if len(player_data) > 1:
-                                    plt.plot(player_data['Timestamp'], player_data[y])
-                                    plot = True
-                        else:
-                            if len(trial_data) > 1:
-                                if not plot:
-                                    plt.figure()
-                                    plot = True
-                                plt.plot(trial_data['Timestamp'], trial_data[y], label=trial)
-                    # if plot:
-                    #     plt.show()
-                    #     plt.savefig(f'/home/david/Downloads/plots/{name}_{y}.png')
-                    #     plt.close()
-        result.to_csv('/home/david/Downloads/ac_stats.csv')
+        columns = self.log_columns
+        if self.config.get('output', 'ac', fallback=False):
+            columns += sorted(self.ac_columns)
+        for col in columns:
+            if col not in self.log_data.columns:
+                self.log_data[col] = numpy.nan
+        fname = self.config.get('output', 'file', fallback='interventions.csv')
+        self.log_data.to_csv(fname, index=False, columns=columns)
+        if self.config.get('output', 'ac', fallback=False):
+            result = pandas.DataFrame(columns=['AC', 'variable', 'README'])
+            new_rows = []
+            for name, data in sorted(self.AC_data.items(), key=lambda tup: tup[0].lower()):
+                if data is not None:
+                    new_rows += [{'AC': name.split('_')[1].lower(), 
+                                  'variable': col, 
+                                  'README': AC_specs[name].get('README', None)}
+                                 for col in data.drop(columns=['ASI', 'score', 'team', 'timestamp', 'trial']).columns]
+                    # numeric = data.drop(columns=non_stats, errors='ignore')
+                    # frame = numeric
+                    # frame = frame.min().to_frame('min')
+                    # frame = frame.join(numeric.max().to_frame('max'))
+                    # frame = frame.join(numeric.mean().to_frame('mean'))
+                    # frame = frame.join(numeric.std().to_frame('std'))
+                    # numeric = self.AC_last[name].drop(columns=non_stats, errors='ignore')
+                    # frame = frame.join(numeric.min().to_frame('min final'))
+                    # frame = frame.join(numeric.max().to_frame('max final'))
+                    # frame = frame.join(numeric.mean().to_frame('mean final'))
+                    # frame = frame.join(numeric.std().to_frame('std final'))
+                    # frame.insert(0, 'AC', name)
+                    result = pandas.concat([result, pandas.DataFrame.from_records(new_rows)], ignore_index=True).drop_duplicates()
+            result = result.sort_values(by=['AC', 'variable'], key=lambda col: col.str.lower())
+            ac_fname = f'{os.path.splitext(fname)[0]}_ac.csv'
+            result.to_csv(ac_fname, index=False)
+        if self.config.getboolean('output', 'compliance', fallback=False):
+            comply_fname = f'{os.path.splitext(fname)[0]}_comp.csv'
+            self.compliance_data.to_csv(comply_fname, index=False)
 
     def parameterized_replay(self, args, simulate=False):
         if args['profile']:
@@ -287,6 +307,7 @@ def find_trial(trial, log_dir):
 
 def replay_parser(files_optional=False):
     parser = ArgumentParser()
+    parser.add_argument('-o', '--output', help='Filename for output')
     parser.add_argument('--config', help='Config file specifying execution parameters')
     parser.add_argument('fname', nargs='*' if files_optional else '+',
                         help='Log file(s) (or directory of log files) to process')
@@ -297,7 +318,17 @@ def replay_parser(files_optional=False):
                         help='Trials to include (default is all)')
     parser.add_argument('-d', '--debug', default='WARNING', help='Level of logging detail')
     parser.add_argument('--profile', action='store_true', help='Run profiler')
+    parser.add_argument('--clean', action='store_true', help='Delete older versions of data files')
     parser.add_argument('--strict', action='store_true', help='Throw exceptions without catching them')
+    parser.add_argument('--ac', action='store_true', help='Add AC variables to the output')
+    parser.add_argument('--decision', action='store_true', help='Add ASI decision inputs to the output')
+    parser.add_argument('--hypothetical', action='store_true', help='Add ASI hypothetical interventions to the output')
+    parser.add_argument('--compliance', action='store_true', help='Add compliance measures to the output')
+    parser.add_argument('--psychsim', action='store_true', help='Save initial PsychSim world')
+    parser.add_argument('--noactual', action='store_true', help='Do not add actual interventions to the output')
+    parser.add_argument('--off', choices=['ac_cmu_ta2_ted', 'ac_cmu_ta2_beard', 'AC_CORNELL_TA2_TEAMTRUST', 'ac_gallup_ta2_gelp',
+                                          'ac_ihmc_ta2_joint-activity-interdependence', 'AC_Rutgers_TA2_Utility',
+                                          'ac_ucf_ta2_playerprofiler'], nargs='+')
     return parser
 
 
@@ -313,8 +344,39 @@ def parse_replay_args(parser, arg_list=None):
     return args
 
 
+def get_transcript_name(fname: str) -> str:
+    """
+    :return: The name of the transcript file associated with the given metadata file
+    """
+    conditions = filename_to_condition(fname)
+    matches = glob.glob(os.path.join(os.path.dirname(fname), f'HSRData_ZoomAudioTranscript_Trial-na_Team-{conditions["Team"]}_Member-{conditions["Member"]}_CondBtwn-{"-".join(conditions["CondBtwn"])}_CondWin-{conditions["CondWin"]}_Vers-*.vtt'))
+    try:
+        return max(matches, key=lambda f: int(filename_to_condition(f)['Vers']))
+    except ValueError:
+        # No matches
+        return None
+
+
 if __name__ == '__main__':
     # Process command-line arguments
     args = parse_replay_args(replay_parser())
-    replayer = Replayer(args['fname'], args['trials'], config=args['config'], strict=args['strict'], logger=logging)
+    config = configparser.ConfigParser()
+    if args['config'] is not None:
+        config.read(config)
+    if 'output' not in config:
+        config['output'] = {'actual': 'yes'}
+    for field in ['ac', 'decision', 'hypothetical', 'compliance', 'psychsim']:
+        if args[field]:
+            config['output'][field] = 'yes'
+    if args['noactual']:
+        config['output']['actual'] = 'no'
+    if args['output']:
+        config['output']['file'] = args['output']
+    if args['off'] is not None:
+        if 'teamwork' not in config:
+            config['teamwork'] = {}
+        for name in args['off']:
+            config['teamwork'][name] = 'no'
+    replayer = Replayer(args['fname'], args['trials'], config=config, 
+                        strict=args['strict'], clean=args['clean'], logger=logging)
     replayer.parameterized_replay(args)
